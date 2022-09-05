5ab5e860a8ced95e7b7ba0f3561dec22a0b6695c
Amadi Jerry <amadijerry310@gmail.com>
2022-09-05 09:05:04 +0100

    added animations with javascript

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const menuIcon = document.querySelector(".hamburger-menu")
 const navbar = document.querySelector(".navbar")
+// const header = document.querySelector(".header")
+let lastScrollTop = 0
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+// window.addEventListener("scroll", function(){
+//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+//     if(scrollTop > lastScrollTop){
+//         header.style.top = "-91px"
+//     }else {
+//         header.style.top = "0"
+//     }
+//     lastScrollTop = scrollTop
+// })
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("change")
 })
+function fadeEffect(heightLimit, className){
+    if(document.body.scrollTop > heightLimit || document.documentElement.scrollTop > heightLimit){
+        document.querySelector(`.${className}`).style.display = "block"
+    }
+}
+function flexedFadeEffect(heightLimit, className){
+    if (document.body.scrollTop > heightLimit || document.documentElement.scrollTop > heightLimit) {
+        document.querySelector(`.${className}`).style.display = "flex"
+    }
+}
+window.addEventListener("scroll", function () { fadeEffect(800, "best-programs") })
+window.addEventListener("scroll", function(){fadeEffect(1000, "weight-loss-card-bg")})
+window.addEventListener("scroll", function(){flexedFadeEffect(1200, "weight-loss-plan")})
