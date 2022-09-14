4c43c90f8f6f611edcbcdc4761852e83ab7402f6
Amadi Jerry <amadijerry310@gmail.com>
2022-09-14 07:25:34 +0100

    replaced fadeEffect function with AOS javascript library

--- script.js
+++ script.js
@@ -3,19 +3,6 @@ const navbar = document.querySelector(".navbar")
 // const email = document.querySelector(".email").value
 const submit = document.querySelector(".submit-btn")
 const error = document.querySelector(".error")
-let lastScrollTop = 0
-let scrollTop = window.pageYOffset || document.documentElement.scrollTop
-// const header = document.querySelector(".header")
-/*window.addEventListener("scroll", function(){
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    if(scrollTop > lastScrollTop){
-        header.style.position = "fixed"
-        header.style.top = "-91px"
-    }else {
-        header.style.top = "0"
-    }
-    lastScrollTop = scrollTop
-})*/
 menuIcon.addEventListener("click", () => {
     menuIcon.style.position = "fixed"
     navbar.classList.toggle("change")
@@ -24,15 +11,6 @@ menuIcon.addEventListener("click", () => {
         menuIcon.style.position = "sticky"
     }
 })
-function fadeEffect(heightLimit, className, displayType) {
-    if (document.body.scrollTop > heightLimit || document.documentElement.scrollTop > heightLimit) {
-        document.querySelector(`.${className}`).style.display = `${displayType}`
-    }
-}
-window.addEventListener("scroll", function () { fadeEffect(800, "best-programs", "block") })
-window.addEventListener("scroll", function () { fadeEffect(1000, "weight-loss-card-bg", "block") })
-window.addEventListener("scroll", function () { fadeEffect(1200, "weight-loss-plan", "flex") })
-window.addEventListener("scroll", function () { fadeEffect(1600, "eating-regular-meals-info-bg", "block") })
 submit.addEventListener("click", function (evt) {
     let email = document.querySelector(".email").value
     let emailAddress = []
